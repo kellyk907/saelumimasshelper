feb53e419b2a71226d1f1633f73029bdd069fa94
Kelly K <pinkflamingo681@gmail.com>
2025-11-23 14:15:27 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'lumi-pink': '#FFB6C1',
+        'lumi-blue': '#ADD8E6',
+      }
+    },
+  },
+  plugins: [],
+}
